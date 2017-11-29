a4da575c539c295a586d57f7d198929af4227748
Todd Oquist <todd@l4digital.com>
2017-11-29 13:20:16 -0800

    Added contained modal examples

--- src/examples/index.js
+++ src/examples/index.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 
 import Overrides from './overrides';
+import Contained from './contained';
 
 const Examples = () => (
     <Router>
@@ -13,11 +14,18 @@ const Examples = () => (
             <h1>Modal Madness</h1>
             <nav>
                 <ul>
-                    <li><Link to="/overrides">Modals with overrides</Link></li>
+                    <li>
+                        <Link to="/overrides">Basic modals with overrides</Link>
+                    </li>
+                    <li>
+                        <Link to="/contained">Contained modals</Link>
+                    </li>
                 </ul>
             </nav>
+            <hr />
             <main>
                 <Route path="/overrides" component={Overrides} />
+                <Route path="/contained" component={Contained} />
             </main>
         </div>
     </Router>
